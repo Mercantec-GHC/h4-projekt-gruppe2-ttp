9a232429ccf870e114b7cfba615086b45741a029
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-02-10 11:55:25 +0100

    home page impl

--- Mobile/lib/home_page.dart
+++ Mobile/lib/home_page.dart
@@ -1,86 +1,224 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/battle_page.dart';
-import 'package:mobile/user_page.dart';
-import 'package:provider/provider.dart';
 import 'package:mobile/battle.dart';
+import 'package:mobile/battle_page.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _HomePage extends StatelessWidget {
+  const _HomePage();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
+    return Column(
       children: [
-        Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color(0xF0D9D9D9),
-              ),
-              width: 325,
-              height: 650,
-              margin: const EdgeInsets.all(40),
-              child: Column(
-                children: [
-                  Text(
-                    "Hjem",
-                    style: TextStyle(fontSize: 48),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    thickness: 3,
-                  ),
-                ],
+        RichText(
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: const <TextSpan>[
+              TextSpan(text: 'Velkommen, ', style: TextStyle(fontSize: 24.0)),
+              TextSpan(
+                  text: '%username%',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
+            ],
+          ),
+        ),
+        Divider(),
+        RichText(
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: const <TextSpan>[
+              TextSpan(
+                  text: 'Gange vundet: ', style: TextStyle(fontSize: 20.0)),
+              TextSpan(
+                  text: '42',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+              TextSpan(text: ' 👑🤫🧏‍♂️', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: const <TextSpan>[
+              TextSpan(text: 'Gange tabt: ', style: TextStyle(fontSize: 20.0)),
+              TextSpan(
+                  text: '99',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+              TextSpan(text: ' 💔🥀😞', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: const <TextSpan>[
+              TextSpan(text: 'Spil i alt: ', style: TextStyle(fontSize: 20.0)),
+              TextSpan(
+                  text: '141',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+              TextSpan(text: ' 🎖️⚔️💥', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        ),
+        RichText(
+          text: TextSpan(
+            style: DefaultTextStyle.of(context).style,
+            children: const <TextSpan>[
+              TextSpan(
+                  text: 'Korrekte svar: ', style: TextStyle(fontSize: 20.0)),
+              TextSpan(
+                  text: '77%',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
+              TextSpan(text: ' 🤓📊✅', style: TextStyle(fontSize: 20.0)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _BattlePage extends StatelessWidget {
+  const _BattlePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Klar til kamp?", style: TextStyle(fontSize: 24.0)),
+        SizedBox(height: 16.0),
+        FilledButton(
+          onPressed: () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (builder) => ChangeNotifierProvider(
+                create: (context) => Battle(),
+                builder: (context, _) => BattlePage(),
               ),
             ),
-          ],
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            color: const Color(0xF0D9D9D9),
-            height: 80,
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Image.asset("house_icon.png"),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Image.asset("vs_icon.png"),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChangeNotifierProvider(
-                        create: (_) => Battle(),
-                        child: BattlePage(),
-                      ),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Image.asset("user_icon.png"),
-                  onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => UserPage(),
-                    ),
-                  ),
-                )
-              ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Kom i gang! ⚔️",
+              style: TextStyle(fontSize: 24.0),
             ),
           ),
         ),
       ],
-    ));
+    );
+  }
+}
+
+class _ProfilePage extends StatelessWidget {
+  const _ProfilePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("%username%", style: TextStyle(fontSize: 24.0)),
+        SizedBox(height: 16.0),
+        FilledButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Lock uij",
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeNavigation extends StatefulWidget {
+  const HomeNavigation({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeNavigationState();
+}
+
+enum _Page {
+  homePage,
+  battlePage,
+  profilePage,
+}
+
+class _CardContainer extends StatelessWidget {
+  final Widget child;
+
+  const _CardContainer({required this.child});
+
+  @override
+  Widget build(BuildContext context) => ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 1000.0),
+        child: SizedBox.expand(
+          child: Card(
+            margin: EdgeInsets.all(20.0),
+            elevation: 4.0,
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: child,
+            ),
+          ),
+        ),
+      );
+}
+
+class _HomeNavigationState extends State<HomeNavigation> {
+  _HomeNavigationState();
+
+  var _index = _Page.homePage;
+
+  void _setPage(_Page page) => setState(() => _index = page);
+
+  double _fontSize(_Page page) => page == _index ? 32 : 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            switch (index) {
+              case 0:
+                return _setPage(_Page.homePage);
+              case 1:
+                return _setPage(_Page.battlePage);
+              case 2:
+                return _setPage(_Page.profilePage);
+              case _:
+                throw Exception("unreachable");
+            }
+          },
+          backgroundColor: Theme.of(context).cardColor,
+          items: [
+            BottomNavigationBarItem(
+                label: "Hjem",
+                icon: Text("🏠",
+                    style: TextStyle(fontSize: _fontSize(_Page.homePage)))),
+            BottomNavigationBarItem(
+                label: "Start kamp",
+                icon: Text("🆚",
+                    style: TextStyle(fontSize: _fontSize(_Page.battlePage)))),
+            BottomNavigationBarItem(
+                label: "Profil",
+                icon: Text("👤",
+                    style: TextStyle(fontSize: _fontSize(_Page.profilePage))))
+          ]),
+      body: Center(
+        child: switch (_index) {
+          _Page.profilePage => _CardContainer(child: _ProfilePage()),
+          _Page.homePage => _CardContainer(child: _HomePage()),
+          _Page.battlePage => _BattlePage(),
+        },
+      ),
+    );
   }
 }
