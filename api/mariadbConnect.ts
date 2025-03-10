c146b2fbee1fb3be8a48df0e9bd42eba97453e49
Phamishan <phamishan93@gmail.com>
2025-03-10 11:22:58 +0100

    fix for NaN value

--- api/mariadbConnect.ts
+++ api/mariadbConnect.ts
@@ -74,9 +74,11 @@ export class MariaDb implements Db {
     stats: InputStats,
   ): Promise<null> {
     const current = await this.getUserStats(username);
-    const correctness = stats.correctanswers / stats.totalanswers;
+    let correctness = stats.correctanswers / stats.totalanswers;
 
-    console.log(correctness);
+    if (Number.isNaN(correctness)) {
+      correctness = 0;
+    }
 
     if (current == null) {
       //creates stat record for user in db with a win
